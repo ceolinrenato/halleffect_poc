d2440a6b1046f599e80cb8fafd807b12cad30c3f
Renato Ceolin <renato.ceolin@renatoceolin.com>
2020-04-02 12:37:08 -0300

    Working

---
+++ src/Main.cpp
@@ -0,0 +1,50 @@
+#include <LiquidCrystal_I2C.h>
+#include <Arduino.h>
+
+#define LCD_COLS 16
+#define LCD_ROWS 2
+#define FUEL_IN_PIN 5
+#define FUEL_OUT_PIN 18
+
+LiquidCrystal_I2C lcd(0x27, LCD_COLS, LCD_ROWS);
+
+volatile int fuelInCounter = 0;
+volatile int fuelOutCounter = 0;
+
+portMUX_TYPE muxIn = portMUX_INITIALIZER_UNLOCKED;
+portMUX_TYPE muxOut = portMUX_INITIALIZER_UNLOCKED;
+
+void IRAM_ATTR handleFuelIn() {
+  portENTER_CRITICAL_ISR(&muxIn);
+  fuelInCounter++;
+  portEXIT_CRITICAL_ISR(&muxIn);
+}
+
+void IRAM_ATTR handleFuelOut() {
+  portENTER_CRITICAL_ISR(&muxOut);
+  fuelOutCounter++;
+  portEXIT_CRITICAL_ISR(&muxOut);
+}
+
+void setup(){
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Total Consumido");
+  lcd.setCursor(14, 1);
+  lcd.print("L");
+  lcd.setCursor(0,1);
+  pinMode(FUEL_IN_PIN, INPUT_PULLUP);
+  pinMode(FUEL_OUT_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(FUEL_IN_PIN), handleFuelIn, RISING);
+  attachInterrupt(digitalPinToInterrupt(FUEL_OUT_PIN), handleFuelOut, RISING);
+}
+
+void loop(){
+  float totalLitersConsumed = ((float) fuelInCounter - (float) fuelOutCounter) / 1000;
+  lcd.setCursor(0,1);
+  lcd.print("              ");
+  lcd.setCursor(0,1);
+  lcd.printf("%.3f", totalLitersConsumed);
+  delay(1000);
+}
